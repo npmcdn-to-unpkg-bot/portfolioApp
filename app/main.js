55e169b802848988e20185ff4b9ab0d8acab5afa
Arturo Wibawa <agwibawa@gmail.com>
2016-01-28 23:37:50 -0800

    update

--- app/main.js
+++ app/main.js
@@ -10,23 +10,22 @@ BACK TO TOP BUTTON
 
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
 		//duration of the top scrolling animation (in ms)
 		scroll_top_duration = 700,
 		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
+		el = $('.cd-top');
 
 	//hide or show the "back to top" link
 	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
-			$back_to_top.addClass('cd-fade-out');
-		}
+
+		var windowTop = $(window).scrollTop();
+
+		( windowTop > offset ) ? el.addClass('cd-is-visible') : el.removeClass('cd-is-visible');
+
 	});
 
 	//smooth scroll to top
-	$back_to_top.on('click', function(event){
+	el.on('click', function(event){
 		event.preventDefault();
 		$('body,html').animate({
 			scrollTop: 0 ,
